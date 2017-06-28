e02ec02f3fe4add8f61f0fa3ba59aef27b31ff71
cassiea12 <c.amantite@gmail.com>
2017-06-28 16:10:48 -0700

    rolling dice function and queryselector for scores

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+// .random outputs a random number between 0 and 6 which may have a decimal
+//  . floor takes that random number and removes the decimal
+// + 1 ensures there will never be a 0
+dice = Math.floor(Math.random() * 6) + 1;
+
+// querySelector('#current-0') finds the element with a specific id in the html page
+// textContent = dice; changes the text inside the element to whatever text is after the =
+// innerHTML = dice; changes the text inside the element to html code
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+
+// Change the css display property of the element with a class of .dice to none so the die does not show
+var x = document.querySelector('#score-0').textContent = dice;
+document.querySelector('.dice').style.display = 'none';
